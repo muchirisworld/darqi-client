08cc4b3ab2bf88b752b34f5da7238cfdc3a169e2
Muchiri <waburimuchiri@gmail.com>
2025-04-25 22:25:13 +0300

    feat: add Header and DashboardLayout components for improved layout structure

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    SidebarProvider
+} from "@/components/ui/sidebar";
+import AppSidebar from "@/components/elements/app-sidebar";
+import Header from './_components/header';
+
+const DashboardLayout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+  return (
+    <SidebarProvider>
+        <AppSidebar />
+        <main>
+            <Header />
+            {children}
+        </main>
+    </SidebarProvider>
+  )
+}
+
+export default DashboardLayout;
